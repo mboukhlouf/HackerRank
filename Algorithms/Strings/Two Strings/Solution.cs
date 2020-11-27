79262d1a79874f93798032a52f6199921ea061af
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2020-11-27 21:29:38 +0100

    Two Strings

---
+++ Algorithms/Strings/Two Strings/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the twoStrings function below.
+    static string twoStrings(string s1, string s2) {
+        var s1CharsSet = new HashSet<char>(s1.ToCharArray());
+        var s2CharsArray = s2.ToCharArray();
+        foreach(char c2 in s2CharsArray) {
+            if(s1CharsSet.Contains(c2)) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    static void Main(string[] args) {
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = twoStrings(s1, s2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
