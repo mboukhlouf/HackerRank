e27232f1593f34ceaa7e4413dfca1cd47166c19f
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2020-12-02 17:50:44 +0100

    Sorting Comparator

---
+++ Interview Preparation Kit/Sorting/Sorting Comparator/Solution.cpp
@@ -0,0 +1,51 @@
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+struct Player {
+    int score;
+    string name;
+};
+
+class Checker{
+public:
+  	// complete this method
+    static int comparator(Player a, Player b)  {
+        int diff = a.score - b.score;
+        if(diff == 0) {
+            return b.name.compare(a.name) > 0 ? 1 : -1;
+        }
+        return diff > 0 ? 1 : -1;
+    }
+};
+
+
+
+
+bool compare(Player a, Player b) {
+    if(Checker::comparator(a,b) == -1)
+        return false;
+    return true;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector< Player > players;
+    string name;
+    int score;
+    for(int i = 0; i < n; i++){
+        cin >> name >> score;
+        Player player;
+        player.name = name, player.score = score;
+        players.push_back(player);
+    }
+    sort(players.begin(), players.end(), compare);
+    for(int i = 0; i < players.size(); i++) {
+        cout << players[i].name << " " << players[i].score << endl;
+    }
+    return 0;
+}
