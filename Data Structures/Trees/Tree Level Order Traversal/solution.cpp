4e30003805166a8652890b3f61d9ae45c95a7bde
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2020-12-06 23:48:16 +0100

    Tree Level Order Traversal

---
+++ Data Structures/Trees/Tree Level Order Traversal/solution.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()) {
+            Node* node = q.front();
+            q.pop();
+            if(node->left != nullptr) {
+                q.push(node->left);   
+            }
+            if(node->right != nullptr) {
+                q.push(node->right);
+            }
+            cout << node->data << " ";
+        }
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.levelOrder(root);
+    return 0;
+}
