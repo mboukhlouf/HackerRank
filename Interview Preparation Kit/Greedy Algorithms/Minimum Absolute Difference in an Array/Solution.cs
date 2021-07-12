7eed9b1997ee53dd66b171c6677d4e2812649928
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2021-07-12 12:07:02 +0100

    Minimum Absolute Difference in an Array

---
+++ Interview Preparation Kit/Greedy Algorithms/Minimum Absolute Difference in an Array/Solution.cs
@@ -0,0 +1,58 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'minimumAbsoluteDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int minimumAbsoluteDifference(List<int> arr)
+    {
+        arr.Sort();
+        int minimum = Math.Abs(arr[0] - arr[1]);
+        for(int i = 1; i < arr.Count - 1; i++)
+        {
+            var difference = Math.Abs(arr[i] - arr[i + 1]);
+            if(difference < minimum)
+            {
+                minimum = difference;
+            }
+        }
+        return minimum;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.minimumAbsoluteDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
